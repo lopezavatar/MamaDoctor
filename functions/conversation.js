5becd5fc8b72ea3e51814d15e4fa391e4695a552
lopezavatar <cristian335@hotmail.co.jp>
2018-03-18 13:43:58 +0300

    S

--- functions/conversation.js
+++ functions/conversation.js
@@ -17,59 +17,31 @@ class Conversation {
 	TellSuggestion()
 	{	
 	    let sick = this.dialogflowApp.getArgument(ARG_SICKNESS);
-		var sickname = name;
-		sick = sick.replace(" ", "_");
 		sick = sick.toLowerCase();
 
         const option = getUserAnsweredQuestOption(sick, questData.allergy);
-
-
 		
-		const options = {
-			uri: "https://www.homeremediesweb.com/"+sick+"_home_remedy.php",
-			transform: function (body) {
-				return cheerio.load(body);
-			}
-		};
+		const product = this.dialogflowApp.buildRichResponse();
 		
-		rp(options)
-		.then(($) => {
-			
-			const product = this.dialogflowApp.buildRichResponse();
-
-			product.addBasicCard(this.dialogflowApp.buildBasicCard(option.value.Summary)
-			.setTitle(option.value.Name)
-			.addButton('Read more', option.value.Link)
-			.setImage(option.value.LinkImage, 'Image alternate text')
-			.setImageDisplay('CROPPED')
-			)
-
-            option.value.Steps.forEach(function(step){
-                product.addSimpleResponse({speech: step,
-                    displayText: ''})
-            });
-
-			$('.entry-content h3').each(function() {
-				if($(this).text().includes("Home"))
-					remedies.push($(this).text());
-			});
-
-            option.value.Products.forEach(function(element){
-				element = element.slice(element.search("Using"));
-				element = element.replace("Using ", "");				
-				
-				product.addSuggestionLink(" To Buy " + element, "https://www.walmart.com/search/?query="+element)
-			    console.log("https://www.walmart.com/search/?query="+element);
-			});
-			
-			this.dialogflowApp.ask(
-				product
-			);
-		})
+		product.addBasicCard(this.dialogflowApp.buildBasicCard(option.value.Summary)
+		.setTitle("Home Remedies for "+option.value.Name)
+		.addButton('Read more', option.value.Link)
+		.setImage(option.value.LinkImage, 'Image alternate text')
+		.setImageDisplay('CROPPED')
+		)
 		
-		.catch((err) => {
-			console.log(err);
+		option.value.Steps.forEach(function(step){
+			product.addSimpleResponse({speech: step})
+        });
+		
+		option.value.Products.forEach(function(element){
+			product.addSuggestionLink(" To Buy " + element, "https://www.walmart.com/search/?query="+element)
 		});
+		
+		
+		this.dialogflowApp.ask(
+		    product
+		);
 	}
 }
 
